ff5a7de57c7b9a4625e9085d72b77e4d81a7bac6
Décio <deciocavalcantedeandrade@gmail.com>
2024-11-13 23:04:54 -0300

    Removendo users e posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index] do
-    collection do
-      get 'rdf'
-      get 'posts'
-      get 'execute_sparql_query'
-    end
-  end
-
   resources :incidents, only: [] do
     collection do
       get 'execute_sparql_query'
